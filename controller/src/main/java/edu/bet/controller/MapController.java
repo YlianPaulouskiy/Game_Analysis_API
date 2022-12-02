bbbbbd208c559e112f468ab36e4ebe5d60eb8e6c
YlianPaulouskiy <paulouskiyulyan@gmail.com>
2022-12-02 16:35:42 +0300

    add MapController

--- controller/src/main/java/edu/bet/controller/MapController.java
+++ controller/src/main/java/edu/bet/controller/MapController.java
@@ -1,5 +1,43 @@
 package edu.bet.controller;
 
+import edu.bet.dto.MapDto;
+import edu.bet.dto.MapTeamDto;
+import edu.bet.service.MapService;
+import lombok.AllArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Validated
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/maps")
 public class MapController {
+
+    private final MapService mapService;
+
+    @GetMapping("/get")
+    public MapTeamDto findOneMap(@NotBlank @RequestParam String mapName,
+                                 @NotBlank @RequestParam String teamName) {
+        return mapService.findByName(mapName,teamName);
+    }
+
+    @GetMapping("/getAll")
+    public List<MapTeamDto> findAll() {
+        return mapService.findAll();
+    }
+
+    @PostMapping("/save")
+    public MapTeamDto save(@Valid @RequestBody MapDto mapDto) {
+        return mapService.save(mapDto);
+    }
+
+    @DeleteMapping("/remove")
+    public void remove(@NotBlank @RequestParam String mapName,
+                       @NotBlank @RequestParam String teamName) {
+        mapService.remove(mapName, teamName);
+    }
 }
